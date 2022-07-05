b8fcdb86b4ebb78e9e16447298d843db335763a6
torikki <kzymm1@gmail.com>
2022-07-05 21:00:38 +0300

    new handlers registered & state storage added

--- src/bot.py
+++ src/bot.py
@@ -1,12 +1,27 @@
 import os
 
 from aiogram import Bot, Dispatcher
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher.filters.builtin import CommandStart
 
-from src.handlers import start
+from src.handlers import (
+    start,
+    new_pill,
+    all_pills,
+    pill_name,
+    pill_time,
+    cancel
+)
+from src.states import Dialog
 
-bot = Bot(os.environ.get('TOKEN'))
-dp = Dispatcher(bot)
+bot = Bot(os.environ.get('BOT_TOKEN'))
+storage = MemoryStorage()
+dispatcher = Dispatcher(bot, storage=storage)
 
 
-dp.register_message_handler(start, CommandStart)
+dispatcher.register_message_handler(start, CommandStart())
+dispatcher.register_message_handler(cancel, lambda message: message.text == 'cancel', state='*')
+dispatcher.register_message_handler(new_pill, lambda message: message.text == 'new', state=None)
+dispatcher.register_message_handler(all_pills, lambda message: message.text == 'all', state=None)
+dispatcher.register_message_handler(pill_name, state=Dialog.new_pill_name)
+dispatcher.register_message_handler(pill_time, state=Dialog.new_pill_time)
