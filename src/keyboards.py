0ce43bf0fcda367c052cdf6b9d0b68675d29094a
torikki <kzymm1@gmail.com>
2022-07-14 18:39:15 +0300

    new keyboards added

--- src/keyboards.py
+++ src/keyboards.py
@@ -22,9 +22,10 @@ async def get_cancel_keyboard() -> ReplyKeyboardMarkup:
 
 async def get_all_pills_keyboards(pills: AsyncIterable) -> InlineKeyboardMarkup:
     keyboard = InlineKeyboardMarkup(1)
+    # TODO: set button limit
     async for pill in pills:
         keyboard.add(
-            InlineKeyboardButton(pill['title'], callback_data='1'),
+            InlineKeyboardButton(pill['title'], callback_data=str(pill["_id"])),
         )
     return keyboard
 
@@ -32,5 +33,24 @@ async def get_all_pills_keyboards(pills: AsyncIterable) -> InlineKeyboardMarkup:
 async def get_save_pill_keyboards() -> ReplyKeyboardMarkup:
     return ReplyKeyboardMarkup(
         [[KeyboardButton('Добавить время'), KeyboardButton('Сохранить')], [KeyboardButton('На главную')]],
+        resize_keyboard=True
+    )
+
+
+async def get_pill_info_keyboard() -> ReplyKeyboardMarkup:
+    return ReplyKeyboardMarkup(
+        [
+            [KeyboardButton('Переименовать')],
+            [KeyboardButton('Удалить время'), KeyboardButton('Добавить время')],
+            [KeyboardButton('Приостановить'), KeyboardButton('Удалить')],
+            [KeyboardButton('На главную')]
+        ],
+        resize_keyboard=True
+    )
+
+
+async def get_delete_approve_keyboard():
+    return ReplyKeyboardMarkup(
+        [[KeyboardButton('Удалить'), KeyboardButton('Отмена')], [KeyboardButton('На главную')]],
         resize_keyboard=True
     )
