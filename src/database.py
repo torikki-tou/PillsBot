26a11e2827a554386fb3a168688e571831533af8
torikki <kzymm1@gmail.com>
2022-07-14 18:38:35 +0300

    new db methods

--- src/database.py
+++ src/database.py
@@ -1,8 +1,10 @@
 import os
+from functools import wraps
 from typing import Callable, List, AsyncIterable
 
 import aiogram.types
 from motor.motor_asyncio import AsyncIOMotorClient
+from bson import ObjectId
 
 
 def _with_mongo_collection(collection_name: str) -> Callable:
@@ -10,6 +12,7 @@ def _with_mongo_collection(collection_name: str) -> Callable:
         client = AsyncIOMotorClient(os.environ.get('MONGO_CONNECTION_STRING'))
         collection = client.pillsbot[collection_name]
 
+        @wraps(func)
         async def wrapper(*args, **kwargs):
             return await func(collection, *args, **kwargs)
         return wrapper
@@ -28,10 +31,16 @@ async def insert_new_pill(collection, user_id: int, pill_title: str, times_to_ta
     pill_to_insert = {
         'user_id': user_id,
         'title': pill_title,
-        'times_to_take': times_to_take
+        'times_to_take': times_to_take,
+        'paused': False,
     }
 
     await collection.insert_one(pill_to_insert)
+
+
+@_with_mongo_collection(collection_name='pills')
+async def get_pill_by_id(collection, pill_id: str) -> dict:
+    return await collection.find_one({'_id': ObjectId(pill_id)})
 
 
 @_with_mongo_collection(collection_name='pills')
@@ -39,3 +48,17 @@ async def get_all_pills_of_user(collection, user_id: int) -> AsyncIterable:
 
     return collection.find({'user_id': user_id})
 
+
+@_with_mongo_collection(collection_name='pills')
+async def delete_pill(collection, pill_id: str) -> None:
+
+    return collection.delete_one({'_id': ObjectId(pill_id)})
+
+
+@_with_mongo_collection(collection_name='pills')
+async def update_pill_title(collection, pill_id: str, new_title: str) -> None:
+
+    return collection.update_one(
+        {'_id': ObjectId(pill_id)},
+        {'$set': {'title': new_title}}
+    )
